c7f0f53f133cd3020629ea4ee4011b3a78771de0
Treasa Janet <67830387+janettrez7@users.noreply.github.com>
2025-06-22 21:40:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,15 +22,12 @@ with open(YOUTUBE_FILE, "r") as f:
 
 st.set_page_config(page_title="Teaching Portal", layout="centered")
 
-# -----------------------
-# LOGIN SECTION
-# -----------------------
 
 if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
 
 if not st.session_state.logged_in:
-    st.title("🔐 Login to Teaching Portal")
+    st.title("🔐 Welcome Tessy, please login")
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
     if st.button("Login"):
